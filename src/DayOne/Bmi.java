678c32b93cfe3b00e2546015aaea0c49cd775c62
My Name <blah@example.co.uk>
2015-02-09 13:44:04 +0000

    bmi extracted to its own method

--- src/DayOne/Bmi.java
+++ src/DayOne/Bmi.java
@@ -9,7 +9,7 @@ public class Bmi {
         System.out.print("Enter weight:");
         float weight = (float)(WellHouseInput.readNumber());
 
-        float BMI = calculateBmi(height, weight);
+        float BMI = getBmi(height, weight);
 
         System.out.println("for height " + height + " and weight " + weight);
         System.out.println("Initial BMI is " + BMI);
@@ -25,10 +25,10 @@ public class Bmi {
 
         int slims = 0;
         while (BMI > 24.9f) {
-            slims = slims + 1;
-            weight = weight - 2.0f;
-            System.out.println("Drop to " + weight + "kgs ... ");
-            BMI = calculateBmi(height,weight);
+            slims ++;
+            weight -= 2.0f;
+            System.out.print("Drop to " + weight + "kgs ... ");
+            BMI = getBmi(height, weight);
             System.out.println("new BMI is " + BMI);
             if (slims > 10) {
                 System.out.println("Check with a doctore");
@@ -38,7 +38,9 @@ public class Bmi {
 
     }
 
-    public static float calculateBmi(float height, float weight) {
-        return (weight / (height * height));
+    private static float getBmi(float height, float weight) {
+        return weight / (height * height);
     }
+
+
 }
