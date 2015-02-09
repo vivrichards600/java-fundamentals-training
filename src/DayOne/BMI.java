e5e3ee05c368e7bfcc755d0e209719f71b561640
My Name <blah@example.co.uk>
2015-02-09 12:14:33 +0000

    bmi check added to advise user on their weight

--- src/DayOne/BMI.java
+++ src/DayOne/BMI.java
@@ -5,13 +5,21 @@ public class BMI {
     public static void main (String [] args) {
 
         System.out.print("Enter height:");
-        float height = (WellHouseInput.readNumber());
+        float height = (float)(WellHouseInput.readNumber());
         System.out.print("Enter height:");
-        float weight = (WellHouseInput.readNumber());
+        float weight = (float)(WellHouseInput.readNumber());
 
-        //TODO: below calculation isn't working as intended!
-        float bmi = weight / (height *  height);
+        float bmi = (weight / (height *  height));
 
-        System.out.println("Persons BMI based on a height of " + height + " and weight of " + weight + " is " + bmi);
+        System.out.println("Your BMI is " + bmi);
+
+        if (bmi > 0) {
+            if (bmi < 18.5) System.out.println("You are underweight. Feed!");
+            if ((18.5 < bmi) && (bmi < 24.9)) System.out.println("You are of normal weight. Keep it up!");
+            if ((25 < bmi) && (bmi < 29.9)) System.out.println("You are overweight. Cool down a bit!");
+            if ((30 < bmi) && (bmi < 34.9)) System.out.println("You are obese, class I. Beware!");
+            if ((35 < bmi) && (bmi < 39.9)) System.out.println("You are obese, class II. You damage yourself!");
+            if (bmi > 40) System.out.println("You are obese, class III. Go see a doctor!");
+        }
     }
 }
