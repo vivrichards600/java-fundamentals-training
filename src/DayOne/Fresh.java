9841ab9fb5292945683e8413d5b471c4cde97ea5
My Name <blah@example.co.uk>
2015-02-09 12:08:40 +0000

    Calculation added for owkring out number of people waiting for a train

--- src/DayOne/Fresh.java
+++ src/DayOne/Fresh.java
@@ -6,9 +6,9 @@ public class Fresh {
 
         //Get the length and seats of a train
 
-        System.out.print("How long?");
+        System.out.print("How long? ");
         int cars = (int)(WellHouseInput.readNumber());
-        System.out.print("How many seats?");
+        System.out.print("How many seats? ");
         int seats = (int)(WellHouseInput.readNumber());
 
         int capacity = (int)(seats * cars * 1.4);
@@ -16,8 +16,17 @@ public class Fresh {
 
         // get the number going to the Millenium Stadium by rail
 
+        System.out.print("How many at the match?");
+        int crowd = (int)(WellHouseInput.readNumber());
+
         // fill a series of trains with the people wanting to go home
 
+        while (crowd > 0)  {
+            System.out.println("Number waiting is " + crowd);
+            System.out.println("Need another train");
+            crowd = crowd - capacity;
+
+        }
 
     }
 }
