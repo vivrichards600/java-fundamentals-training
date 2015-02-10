ef26a6f25fe82999f9d13ba7920892b58f896a23
My Name <blah@example.co.uk>
2015-02-10 11:45:13 +0000

    bmi now accepts array of people to work out bmi

--- src/DayTwo/BMI.java
+++ src/DayTwo/BMI.java
@@ -1,7 +1,5 @@
 package DayTwo;
 
-import DayOne.WellHouseInput;
-
 public class BMI {
 
     String fullname;
@@ -9,7 +7,6 @@ public class BMI {
     float weight;
 
     BMI(String fullname, double height, float weight) {
-
         this.fullname = fullname;
         this.height= (float)height;
         this.weight = (float)weight;
@@ -22,10 +19,17 @@ public class BMI {
 
     //Test
     public static void main (String [] args) {
-        BMI person = new BMI("Viv",1.79,79);
 
-        float bmiResult = person.getBMI();
-        System.out.println("BMI for " + person.fullname + " with height of " + (float)person.height + " and weight " + person.weight + " is " + bmiResult);
+        BMI [] Celebrity = new BMI[4];
+        Celebrity[0] = new BMI("Viv",1.42,65);
+        Celebrity[1] = new BMI("Dave",1.79,79);
+        Celebrity[2] = new BMI("Steve",1.88,81);
+        Celebrity[3] = new BMI("Mark",1.63,77);
+
+        for (int data=0; data<Celebrity.length; data++){
+            float bmiResult = Celebrity[data].getBMI();
+            System.out.println("BMI for " + Celebrity[data].fullname + " with height of " + (float)Celebrity[data].height + " and weight " + Celebrity[data].weight + " is " + bmiResult);
+        }
     }
 }
 
